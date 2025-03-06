d568e894525e9ad511e2c66182ec0626db4586d1
Sry Kimsour <soursour3001@gmail.com>
2025-03-06 15:45:23 +0700

    haha

--- exercise-5/src/App.jsx
+++ exercise-5/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./index.css";
-import data from "./data";
+import cakes from "./data";
 
-const Card = ({ person }) => {
+const CakeCard = ({ cake }) => {
     return (
         <div className="card">
-            <img src={person.image} alt={person.name} className="card-image" />
-            <h2>{person.name}</h2>
-            <p>{person.class}</p>
-            <p>{person.hobbies}</p>
+            <img src={cake.image} alt={cake.name} className="card-image" />
+            <h2>{cake.name}</h2>
+            <p className="type">{cake.type}</p>
+            <p>{cake.description}</p>
         </div>
     );
 };
@@ -16,9 +16,12 @@ const Card = ({ person }) => {
 const App = () => {
     return (
         <div className="app">
-            {data.map((person, index) => (
-                <Card key={index} person={person} />
-            ))}
+            <h1>Delicious Cakes 🍰</h1>
+            <div className="card-container">
+                {cakes.map((cake, index) => (
+                    <CakeCard key={index} cake={cake} />
+                ))}
+            </div>
         </div>
     );
 };
---
+++ exercise-5/src/data.js
@@ -0,0 +1,28 @@
+const cakes = [
+    {
+        name: "Chocolate Cake",
+        type: "Chocolate",
+        description: "Rich and moist chocolate cake with creamy frosting.",
+        image: "/chocolate_cake.jpg"
+    },
+    {
+        name: "Red Velvet Cake",
+        type: "Red Velvet",
+        description: "A classic red velvet cake with cream cheese frosting.",
+        image: "/red_velvet.jpg"
+    },
+    {
+        name: "Strawberry Shortcake",
+        type: "Fruit",
+        description: "Light sponge cake with fresh strawberries and whipped cream.",
+        image: "/strawberry_shortcake.jpg"
+    },
+    {
+        name: "Vanilla Sponge Cake",
+        type: "Vanilla",
+        description: "Soft and fluffy vanilla sponge cake with buttercream.",
+        image: "/vanilla_spongecake.jpg"
+    }
+];
+
+export default cakes;
